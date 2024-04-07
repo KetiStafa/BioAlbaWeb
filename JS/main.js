05019c48071bb19cb9d9ca8eaa5c6eb0f508550e
agoga21 <rurunikenshin1@gmail.com>
2024-04-07 17:08:21 +0200

    about us

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+
+function initMap() {
+    const myLatLng = { lat: 41.27421429347341, lng: 19.969875355652274150 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 11,
+      center: myLatLng,
+    });
+
+     marker = new google.maps.Marker({
+      position: {lat: 41.2173910964184, lng: 19.969875355652274150},
+      map: map,
+      title: "Hello World!",
+    });
+}
+//41.27421429347341, 19.88066919775938
+
+
+
