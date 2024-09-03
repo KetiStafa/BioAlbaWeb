c6a87e617bc5cf5c1857168711d7a15630364f48
agoga21 <rurunikenshin1@gmail.com>
2024-09-03 15:24:24 +0200

    updates

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function () {
     const navBar = document.querySelector('.nav-bar');
-    if (window.scrollY > 800) {
+    if (window.scrollY > 500) {
         navBar.classList.add('scrolled');
     } else {
         navBar.classList.remove('scrolled');
--- JS/verticalCarousel.js
+++ JS/verticalCarousel.js
@@ -1,68 +1,68 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+// document.addEventListener("DOMContentLoaded", (event) => {
 
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+//     gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-    const list = document.querySelector(".carousel__nav");
-    const listItems = gsap.utils.toArray(".carousel__nav__item", list);
-    const slides = gsap.utils.toArray(".carousel__item");
-    const tl = gsap.timeline({
-        //onUpdate: () => console.log("update", tl.time())
-    });
-    const myST = ScrollTrigger.create({
-        animation: tl,
-        id: "st",
-        trigger: ".our-work",
-        start: "top top",
-        end: "+=500%",
-        pin: ".our-work",
-        scrub: true,
-        snap: {
-            snapTo: 1 / (slides.length)
-        },
-        markers: false
-    })
+//     const list = document.querySelector(".carousel__nav");
+//     const listItems = gsap.utils.toArray(".carousel__nav__item", list);
+//     const slides = gsap.utils.toArray(".carousel__item");
+//     const tl = gsap.timeline({
+//         //onUpdate: () => console.log("update", tl.time())
+//     });
+//     const myST = ScrollTrigger.create({
+//         animation: tl,
+//         id: "st",
+//         trigger: ".our-work",
+//         start: "top top",
+//         end: "+=500%",
+//         pin: ".our-work",
+//         scrub: true,
+//         snap: {
+//             snapTo: 1 / (slides.length)
+//         },
+//         markers: false
+//     })
 
-    gsap.set(slides, { yPercent: 125, scale: 0.5, opacity: 0 });
-    listItems.forEach((item, i) => {
-        item.addEventListener("click", e => {
-            e.preventDefault();
-            const percent = tl.labels[e.target.getAttribute("data-target")] / tl.totalDuration();
-            const scrollPos = myST.start + (myST.end - myST.start) * percent;
-            gsap.to(window, { duration: 2, scrollTo: scrollPos });
-        });
+//     gsap.set(slides, { yPercent: 125, scale: 0.5, opacity: 0 });
+//     listItems.forEach((item, i) => {
+//         item.addEventListener("click", e => {
+//             e.preventDefault();
+//             const percent = tl.labels[e.target.getAttribute("data-target")] / tl.totalDuration();
+//             const scrollPos = myST.start + (myST.end - myST.start) * percent;
+//             gsap.to(window, { duration: 1, scrollTo: scrollPos });
+//         });
 
-        const previousItem = listItems[i - 1];
-        if (previousItem) {
-            tl
-                .to(item, { background: "#369A7E", boxShadow: '0 0 16px white' }, 0.5 * (i - 1))
-                .to(
-                    slides[i],
-                    {
-                        opacity: 1,
-                        yPercent: 0,
-                        scale: 1,
-                    },
-                    '<'
-                )
-                .to(previousItem, { backgroundColor: '#7f848c', boxShadow: '0 0 16px transparent' }, '<')
-                .to(
-                    slides[i - 1],
-                    {
-                        opacity: 0,
-                        yPercent: -125,
-                        scale: 0.5,
-                    },
-                    '<'
-                ).add("our-work-" + (i + 1))
-        } else {
-            gsap.set(item, { background: "#369A7E", boxShadow: '0 0 16px white' });
-            tl.to(slides[i], {
-                yPercent: 0,
-                opacity: 1,
-                scale: 1,
-                duration: 0,
-            }, 0);
-            tl.add("our-work-" + (i + 1), "+=0.5")
-        }
-    });
-});
+//         const previousItem = listItems[i - 1];
+//         if (previousItem) {
+//             tl
+//                 .to(item, { background: "white", boxShadow: '0 0 16px white' }, 0.5 * (i - 1))
+//                 .to(
+//                     slides[i],
+//                     {
+//                         opacity: 1,
+//                         yPercent: 0,
+//                         scale: 1,
+//                     },
+//                     '<'
+//                 )
+//                 .to(previousItem, { backgroundColor: '#7f848c', boxShadow: '0 0 16px transparent' }, '<')
+//                 .to(
+//                     slides[i - 1],
+//                     {
+//                         opacity: 0,
+//                         yPercent: -125,
+//                         scale: 0.5,
+//                     },
+//                     '<'
+//                 ).add("our-work-" + (i + 1))
+//         } else {
+//             gsap.set(item, { background: "white", boxShadow: '0 0 16px white' });
+//             tl.to(slides[i], {
+//                 yPercent: 0,
+//                 opacity: 1,
+//                 scale: 1,
+//                 duration: 0,
+//             }, 0);
+//             tl.add("our-work-" + (i + 1), "+=0.5")
+//         }
+//     });
+// });
